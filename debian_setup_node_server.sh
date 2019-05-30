5269a6a7660bdba46c73e3f8446c35924c2763f1
Cris Stringfellow <3038633-dosycorp@users.noreply.gitlab.com>
2019-05-30 03:42:56 -0500

    "OK"

--- debian_setup_node_server.sh
+++ debian_setup_node_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt -y install curl nodejs vim 
+sudo apt -y install curl nodejs certbot vim 
 git config --global core.editor "vim"
 sudo update-alternatives --config editor
 curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
@@ -11,6 +11,7 @@ curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o
 bash ./install_nvm.sh
 source $HOME/.profile
 nvm install --lts
-
-
+sudo apt -y install dnsutils whois
+sudo apt autoremove
+sudo certbot certonly --manual --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --manual-public-ip-logging-ok -d "*.$1" -d $1
 
