361f6b71ae0795d9a86022374f4c4a9f32675a15
Cris Stringfellow <cris@dosyago.com>
2019-10-27 08:47:37 +0000

    "OK"
    Former-commit-id: e8840cc202b6830d0103926c0c0f231d63c55842

--- debian_setup_node_server.sh
+++ debian_setup_node_server.sh
@@ -22,3 +22,4 @@ sudo apt -y install dnsutils whois
 sudo apt autoremove
 sudo certbot certonly --manual --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --manual-public-ip-logging-ok -d "*.$1" -d $1
 sudo npm i -g serve nodemon
+sudo apt install psmisc htop nethogs
