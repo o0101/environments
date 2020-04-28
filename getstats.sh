baa2a31c9321dc73d2d4e99f0638ec43e29d4972
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-04-28 12:51:20 +0000

    "Updated getstats to save previous"

--- getstats.sh
+++ getstats.sh
@@ -1,5 +1,5 @@
-rm -rf ./stats
-mkdir stats
+mkdir -p stats/$(date -I)
+mv stats/code* stats/$(date -I)
 sloc --exclude "node_modules|bundle" . > stats/codestats.txt
 cloc ./ --exclude-dir=node_modules --exclude-ext=json --use-sloccount --csv --by-file --report-file stats/codecount.csv
 
