6187c59c8f7343bfd5392933da10f8bad5436a5a
Cris <cris7fe@gmail.com>
2021-09-22 11:25:15 +0800

    Update debian_setup_node_server.sh

--- debian_setup_node_server.sh
+++ debian_setup_node_server.sh
@@ -5,6 +5,7 @@ if [ -z "$1" ]
     echo "Need to add 1 argument: domain name"
 fi 
 ./basic_setup
+sudo apt install git curl wget
 git config --global user.email "22254235+crislin2046@users.noreply.github.com"
 git config --global user.name "Cris Stringfellow"
 git config --global core.editor "vim"
