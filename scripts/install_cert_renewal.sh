70dc1a8c4bc8b5d48b10f9e1286b1864186df004
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-27 05:45:43 +0000

    "OK"

--- scripts/install_cert_renewal.sh
+++ scripts/install_cert_renewal.sh
@@ -13,8 +13,8 @@ fi
 
 sudo cp ./renew_tls.sh /root/
 # add our job to crontab if it's not there already
-(sudo crontab -l | grep "tls_renew.sh $1") || ( sudo crontab -l; echo "0 12 * * * root /root/tls_renew.sh $1 $2"; ) | sudo crontab
+(sudo crontab -l | grep "tls_renew.sh $1") || ( sudo crontab -l; echo "0 12 * * * /root/tls_renew.sh $1 $2"; ) | sudo crontab
 
 # test, runs every 2 minutes haha
-#(sudo crontab -l | grep "tls_renew.sh $1") || ( sudo crontab -l; echo "*/2 * * * * root /root/tls_renew.sh $1 $2"; ) | sudo crontab
+#(sudo crontab -l | grep "tls_renew.sh $1") || ( sudo crontab -l; echo "*/2 * * * * /root/tls_renew.sh $1 $2"; ) | sudo crontab
 
