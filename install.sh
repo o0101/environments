4a7330d08595c9bbfabb59f1c3df78f4a288863a
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-02-16 05:16:42 +0000

    "Rename"

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt install git
-git clone https://github.com/i5ik/environments.git
+git clone https://github.com/crisdosyago/environments.git
 cd environments/
 ./debian_setup_node_server.sh $1
