2efdb84c857ad414dbbf34e56408716d0f776523
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-02-25 12:44:42 +0800

    "Better vnc"

--- scripts/setup_vnc.sh
+++ scripts/setup_vnc.sh
@@ -2,28 +2,38 @@
 
 me=$(whoami)
 sudo apt install xfce4 xfce4-goodies tightvncserver
+sudo systemctl clean vncserver
 read -p "Set up a password now"
 vncserver
 vncserver -kill :1
 grep -qxF "startxfce4 &" $HOME/.vnc/xstartup || echo "startxfce4 &" >> $HOME/.vnc/xstartup
+echo "
+#!/bin/bash
+xrdb $HOME/.Xresources
+autocutsel -fork
+vncconfig -iconic &
+vncconfig -nowin &
+startxfce4 &
+" > $HOME/.vnc/xstartup
+chmod +x $HOME/.vnc/xstartup
 sudo tee <<EOF  /etc/systemd/system/vncserver.service >/dev/null
 [Unit]
-Description=TightVNC server
+Description=Remote desktop service (VNC)
 After=syslog.target network.target
 
 [Service]
 Type=forking
-User=cris
-Group=cris
-WorkingDirectory=/home/cris
-PIDFile=/home/cris/.vnc/%H:%i.pid
-ExecStartPre=/bin/bash -c "/usr/bin/vncserver -kill :%i > /dev/null 2>&1 || :"
-ExecStart=/usr/bin/vncserver -depth 24 -geometry 1390x750 -localhost :%i
-ExecStop=/usr/bin/vncserver -kill :%i
+User=$USER
+
+# Clean any existing files in /tmp/.X11-unix environment
+ExecStartPre=/bin/sh -c '/usr/bin/vncserver -kill :1 > /dev/null 2>&1 || :'
+ExecStart=/usr/bin/vncserver -geometry 1392x750 -depth 16 -dpi 120 -localhost :1 
+ExecStop=/usr/bin/vncserver -kill :1
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
 sudo systemctl daemon-reload
 sudo systemctl enable --now vncserver
 
